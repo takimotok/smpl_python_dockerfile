03bff1c1b8ee51fa02b9a0899a755f5255d1355e
ktakimoto <takimoto.kengo.dev@gmail.com>
2021-07-22 16:10:59 +0900

    change sqlite3 の練習

--- index.py
+++ index.py
@@ -1,44 +1,51 @@
-import optparse
+import sqlite3
 
-def main():
-    usage = 'usage: %prog [options] arg1 arg2'
-    parser = optparse.OptionParser(usage=usage)
+# create DB as a file
+# conn = sqlite3.connect('test_sqlite.db')
+conn = sqlite3.connect(':memory:')
 
-    parser.add_option(
-        # option
-        '-f', '--file',
-        # parser にどんな動作をさせたいか
-        action='store',
-        # 引数の型
-        type='string',
-        # optparse に知らせる action の値の格納場所
-        dest='filename',
-        # --help 実行時の説明文
-        help='sample descritption about the -f option.'
-    )
+cursor = conn.cursor()
 
-    parser.add_option(
-        '-n', '--num',
-        action='store',
-        type='int',
-        dest='num',
-        help='sample descritption for -n option.'
-    )
+# create table
+sql_create_table = """
+create table persons(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name STRING
+)
+"""
+cursor.execute(sql_create_table)
+conn.commit()
 
-    parser.add_option(
-        '-v', '--verbose',
-        action='store_false',
-        # type を指定したら怒られた
-        # type='bool',
-        dest='verbose',
-        help='sample descritption for -v option.',
-        default=True
-    )
+# insert datum
+sql_insert_datum = """
+INSERT INTO persons (name) VALUES
+('james'),
+('ann'),
+('catharin')
+"""
+cursor.execute(sql_insert_datum)
+conn.commit()
+sql_select = """
+SELECT * FROM persons
+"""
+cursor.execute(sql_select)
+print(cursor.fetchall()) # [(1, 'james'), (2, 'ann'), (3, 'catharin')]
 
-    # unpack touple
-    options, args = parser.parse_args()
-    print(options)
-    print(args)
+# update datum
+sql_udpate = """
+UPDATE persons SET name = 'FOO' WHERE id = 1
+"""
+cursor.execute(sql_udpate)
+conn.commit()
+print(cursor.fetchall()) # [(1, 'Foo'), (2, 'ann'), (3, 'catharin')]
 
-if __name__ == '__main__':
-    main()
+# delete
+sql_delete = """
+DELETE FROM persons WHERE id = 2
+"""
+cursor.execute(sql_delete)
+conn.commit()
+print(cursor.fetchall()) # [(1, 'Foo'), (3, 'catharin')]
+
+cursor.close()
+conn.close()
